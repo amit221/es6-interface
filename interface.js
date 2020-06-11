c280fad48f139bda79629e930e2bd050d758e8d7
amit21 <amit@valhalla.co.il>
2020-06-11 13:56:39 +0300

    an interface can now be an object

--- interface.js
+++ interface.js
@@ -1,107 +1,119 @@
 const getClassMethodNames = (obj) => {
-    let methods = new Set();
-    while (obj = Reflect.getPrototypeOf(obj)) {
-        let keys = Reflect.ownKeys(obj);
-        keys.forEach((k) => methods.add(k));
-    }
-    return methods;
+	let methods = new Set();
+	while (obj = Reflect.getPrototypeOf(obj)) {
+		let keys = Reflect.ownKeys(obj);
+		keys.forEach((k) => methods.add(k));
+	}
+	return methods;
 };
 const getArgs = (func) => {
-    let args = null;
+	let args = null;
 
-    switch (typeof func) {
-        case 'function': {
-            args = func.toString().match(/\(\s*([^)]*?)\s*\)/);
-            break;
-        }
-        case 'string': {
-            args = func.match(/\(\s*([^)]*?)\s*\)/);
-            break;
-        }
-        default: {
-            throw new Error('func must be of type function or string');
-        }
-    }
+	switch (typeof func) {
+		case 'function': {
+			args = func.toString().match(/\(\s*([^)]*?)\s*\)/);
+			break;
+		}
+		case 'string': {
+			args = func.match(/\(\s*([^)]*?)\s*\)/);
+			break;
+		}
+		default: {
+			throw new Error('func must be of type function or string');
+		}
+	}
 
-    if (args === null || !args[1]) {
-        return "";
-    }
-    args = args[1];
-    return args.split(/\s*,\s*/).map((arg) => {
-        return arg.replace(/\/\*.*\*\//, '').trim();
-    }).filter(arg => {
-        return arg;
-    });
+	if (args === null || !args[1]) {
+		return "";
+	}
+	args = args[1];
+	return args.split(/\s*,\s*/).map((arg) => {
+		return arg.replace(/\/\*.*\*\//, '').trim();
+	}).filter(arg => {
+		return arg;
+	});
 };
 
 
 class DummyClassForExtendsIfNoClassWasSendToAvoidError {
 }
 
+function getMethodsFromObject(obj) {
+	return Object.keys(obj).filter(funcName => typeof obj[funcName] === "function").map(funcName => {
+		const funcStr = obj[funcName].toString();
+		if (funcStr.includes("function")) {
+			return funcName + funcStr.substr(0, funcStr.lastIndexOf("{")).replace("function", "");
+		} else if (funcStr.includes("(")) {
+			return funcName + funcStr.substr(funcStr.indexOf("("), funcStr.indexOf("="));
+		} else {
+			return funcName + "(" + funcStr.substr(0, funcStr.indexOf("=")) + ")";
+		}
+	});
+}
 
 const container = function (options) {
 
-    if (container.arguments.length === 0) {
-        throw new Error('you need to supply at least one interface');
-    }
+	if (container.arguments.length === 0) {
+		throw new Error('you need to supply at least one interface');
+	}
 
-    let arr = [];
-    const argLength = container.arguments.length;
-    let Class = null;
+	let arr = [];
+	const argLength = container.arguments.length;
+	let Class = null;
 
-    for (let i in container.arguments) {
+	for (let i in container.arguments) {
 
-        if (container.arguments[i] instanceof Set) {
-            arr = [...arr, ...container.arguments[i]];
-        }
-        else if (parseInt(i) + 1 === argLength && typeof container.arguments[i] === "function") {
-            Class = container.arguments[i];
-        }
-        else {
-            throw new Error("interfaces need to be instance of Set");
-        }
-    }
+		if (container.arguments[i] instanceof Set || Array.isArray(container.arguments[i])) {
+			arr = [...arr, ...container.arguments[i]];
+		} else if (typeof container.arguments[i] === "object" && container.arguments[i] !== null) {
+			arr = [...arr, ...getMethodsFromObject(container.arguments[i])];
+		} else if (parseInt(i) + 1 === argLength && typeof container.arguments[i] === "function") {
+			Class = container.arguments[i];
+		} else {
+			throw new Error("interfaces can be an Array Set or Object");
+		}
+	}
 
-    let requiredMethods = new Set(arr);
-    if (Class === null) {
-        Class = DummyClassForExtendsIfNoClassWasSendToAvoidError;
-    }
+	const requiredMethods = new Set(arr);
+	if (Class === null) {
+		Class = DummyClassForExtendsIfNoClassWasSendToAvoidError;
+	}
 
-    class Interface extends Class {
+	class Interface extends Class {
 
-        constructor(options) {
-            super(options);
+		constructor(options) {
+			super(options);
 
-            const ClassMethods = getClassMethodNames(this);
-            let errors = "";
-            requiredMethods.forEach(key => {
-                let methodNameIndex = key.indexOf("(");
-                let methodName = methodNameIndex > -1 ? key.substr(0, methodNameIndex) : key;
+			const ClassMethods = getClassMethodNames(this);
+			let errors = "";
+			requiredMethods.forEach(key => {
+				let methodNameIndex = key.indexOf("(");
+				let methodName = methodNameIndex > -1 ? key.substr(0, methodNameIndex) : key;
+				methodName = methodName.trim();
+				if (ClassMethods.has(methodName) === false || getArgs(this[methodName]).toString() !== getArgs(key).toString()) {
+					errors += `${methodName}(${getArgs(key)})`.replace(/\s+/g, '') + " ";
+				}
+			});
 
-                if (ClassMethods.has(methodName) === false || getArgs(this[methodName]).toString() !== getArgs(key).toString()) {
-                    errors += `${methodName}(${getArgs(key)}) `;
-                }
-            });
+			if (errors) {
+				throw  new Error(this.constructor.name + ' must implement ' + errors + 'methods');
+			}
+		}
+	}
 
-            if (errors) {
-                throw  new Error(this.constructor.name + ' must implement ' + errors + 'methods');
-            }
-        }
-    }
-
-    return Interface;
+	return Interface;
 };
 
 if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
-    module.exports = container;
-    return;
+	module.exports = container;
+	return;
 }
 
 if (typeof define === 'function' && define.amd) {
-    define([], function () {
-        return container;
-    });
-    return;
+	define([], function () {
+		return container;
+	});
+	return;
 }
 
 window.es6Interface = container;
--- test/interface.js
+++ test/interface.js
@@ -1,112 +1,188 @@
-'use strict'
+'use strict';
 
 const chai = require('chai');
 const expect = chai.expect;
 const Interface = require('../interface');
 
 const testInterface1 = new Set(['required1(arg1)']);
+const testInterface1Object = {
+	required1: arg1 => {
+	}
+};
 const testInterface2 = new Set(['required2(arg1,arg2)', 'required3({arg1 , arg2 , arg3})']);
+const testInterface2Object = {
+	required2: function (arg1, arg2) {
+	},
+	required3: ({arg1, arg2, arg3}) => {
+	}
+};
 const testInterface3 = new Set(['missingArgumentMethod(arg1)']);
+const testInterface3Object = {
+	missingArgumentMethod: function (arg1) {
+	}
+};
 const testInterface4 = new Set(['required4({arg1, arg2, arg3}, arg4)']);
+const testInterface4Object = {
+	required4: function ({arg1, arg2, arg3}, arg4) {
+	}
+};
 
 class parentClass {
-    constructor() {
+	constructor() {
 
-    }
+	}
 
-    required4({arg1, arg2, arg3}, arg4) {
+	required4({arg1, arg2, arg3}, arg4) {
 
-    }
+	}
 }
 
 
 describe('Interface', () => {
 
-    it('should throw error if no interfaces supplied ', () => {
-        try {
-            class testClass extends Interface() {
-                constructor() {
-                    super();
-                }
-            }
+	it('should throw error if no interfaces supplied ', () => {
+		try {
+			class testClass extends Interface() {
+				constructor() {
+					super();
+				}
+			}
 
-        }
-        catch (err) {
-            expect(err.message).to.equal("you need to supply at least one interface")
-        }
-    });
-    it('should throw error if interface is not instance of Set ', () => {
-        try {
-            class testClass extends Interface('STRING') {
-                constructor() {
-                    super();
-                }
-            }
+		} catch (err) {
+			expect(err.message).to.equal("you need to supply at least one interface");
+		}
+	});
+	it('should throw error if interface is not instance of Set Array ot Object', () => {
+		try {
+			class testClass extends Interface('STRING') {
+				constructor() {
+					super();
+				}
+			}
 
-            new testClass();
-        }
-        catch (err) {
-            expect(err.message).to.equal("interfaces need to be instance of Set")
-        }
-    });
-    it('should throw error class did not implemented testInterface1 interface ', () => {
-        try {
-            class testClass extends Interface(testInterface1) {
-                constructor() {
-                    super();
-                }
-            }
-            new testClass();
-        }
-        catch (err) {
-            expect(err.message).to.equal("testClass must implement required1(arg1) methods")
-        }
-    });
-    it('should throw error class did not implemented testInterface3 interface ', () => {
-        try {
-            class testClass extends Interface(testInterface3) {
-                constructor() {
-                    super();
-                }
+			new testClass();
+		} catch (err) {
+			expect(err.message).to.equal("interfaces can be an Array Set or Object");
+		}
+	});
+	it('should throw error class did not implemented testInterface1 interface ', () => {
+		try {
+			class testClass extends Interface(testInterface1) {
+				constructor() {
+					super();
+				}
+			}
 
-                required6() {
-                }
-            }
-            new testClass();
-        }
-        catch (err) {
-            expect(err.message).to.equal("testClass must implement missingArgumentMethod(arg1) methods")
-        }
-    });
+			new testClass();
+		} catch (err) {
+			expect(err.message).to.equal("testClass must implement required1(arg1) methods");
+		}
+	});
+	it('should throw error class did not implemented testInterface1Object interface ', () => {
+		try {
+			class testClass extends Interface(testInterface1Object) {
+				constructor() {
+					super();
+				}
+			}
 
-    it('should throw error class did not implemented testInterface1 and testInterface2 interface ', () => {
-        try {
-            class testClass extends Interface(testInterface1, testInterface2) {
-                constructor() {
-                    super();
-                }
-            }
-            new testClass();
-        }
-        catch (err) {
-            expect(err.message).to.equal("testClass must implement required1(arg1) required2(arg1,arg2) required3({arg1,arg2,arg3}) methods")
-        }
-    })
+			new testClass();
+		} catch (err) {
+			expect(err.message).to.equal("testClass must implement required1(arg1) methods");
+		}
+	});
 
-    it('should throw error class did not implemented testInterface1 and testInterface2 interface ', () => {
-        try {
-            class testClass extends Interface(testInterface1, testInterface2, testInterface4, parentClass) {
-                constructor() {
-                    super();
-                }
 
-            }
-            new testClass();
-        }
-        catch (err) {
-            expect(err.message).to.equal("testClass must implement required1(arg1) required2(arg1,arg2) required3({arg1,arg2,arg3}) methods")
-        }
-    })
+	it('should throw error class did not implemented testInterface3 interface ', () => {
+		try {
+			class testClass extends Interface(testInterface3) {
+				constructor() {
+					super();
+				}
+
+				required6() {
+				}
+			}
+
+			new testClass();
+		} catch (err) {
+			expect(err.message).to.equal("testClass must implement missingArgumentMethod(arg1) methods");
+		}
+	});
+	it('should throw error class did not implemented testInterface3Object interface ', () => {
+		try {
+			class testClass extends Interface(testInterface3Object) {
+				constructor() {
+					super();
+				}
+
+				required6() {
+				}
+			}
+
+			new testClass();
+		} catch (err) {
+			expect(err.message).to.equal("testClass must implement missingArgumentMethod(arg1) methods");
+		}
+	});
+
+
+	it('should throw error class did not implemented testInterface1 and testInterface2 interface ', () => {
+		try {
+			class testClass extends Interface(testInterface1, testInterface2) {
+				constructor() {
+					super();
+				}
+			}
+
+			new testClass();
+		} catch (err) {
+			expect(err.message).to.equal("testClass must implement required1(arg1) required2(arg1,arg2) required3({arg1,arg2,arg3}) methods");
+		}
+	});
+	it('should throw error class did not implemented testInterface1Object and testInterface2Object interface ', () => {
+		try {
+			class testClass extends Interface(testInterface1Object, testInterface2Object) {
+				constructor() {
+					super();
+				}
+			}
+
+			new testClass();
+		} catch (err) {
+			expect(err.message).to.equal("testClass must implement required1(arg1) required2(arg1,arg2) required3({arg1,arg2,arg3}) methods");
+		}
+	});
+
+
+	it('should throw error class did not implemented testInterface1 and testInterface2 interface ', () => {
+		try {
+			class testClass extends Interface(testInterface1, testInterface2, testInterface4, parentClass) {
+				constructor() {
+					super();
+				}
+
+			}
+
+			new testClass();
+		} catch (err) {
+			expect(err.message).to.equal("testClass must implement required1(arg1) required2(arg1,arg2) required3({arg1,arg2,arg3}) methods");
+		}
+	});
+	it('should throw error class did not implemented testInterface1Object and testInterface2Object interface ', () => {
+		try {
+			class testClass extends Interface(testInterface1Object, testInterface2Object, testInterface4Object, parentClass) {
+				constructor() {
+					super();
+				}
+
+			}
+
+			new testClass();
+		} catch (err) {
+			expect(err.message).to.equal("testClass must implement required1(arg1) required2(arg1,arg2) required3({arg1,arg2,arg3}) methods");
+		}
+	});
 
 
 });
